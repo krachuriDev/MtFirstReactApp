0a9a9f04bae72191f72421794a657eae5d5b3660
Karthik Rachuri <karthik.rachuri@knipper.com>
2023-07-19 15:58:52 +0530

    Laying the Foundation/Learnig the Basics

--- App.js
+++ App.js
@@ -1,23 +1,107 @@
-{/* <div id="parent">
-  <div id="child">
-    <h1>I'm an h1 tag</h1>
-    <h2>I'm and h2 tag</h2>
-  </div>
-  <div id="child2">
-    <h1>I'm and h1 tag</h1>
-    <h2>I'm an h2 tag</h2>
-  </div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", { id: 'heading' }, "Hello World from React");
-// console.log(heading);
+/*
+AppLayout
+  Header
+  -Logo
+  -Navigation Items.
+  Body
+  -Search Component
+  -Restro Component
+    -Restro Card
+      -Logo.
+      -Name.
+      -Cuisine.
+      -Rating.
+      -Delivery Time.
+  -Pagination
+  Footer
+  CopyRight
+  Various links like Socialmedia,Contact Us etc
+*/
 
-const parent = React.createElement("div", { id: "parent" },
-  [
-    React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]),
-    React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")])
-  ]
-)
+const Restro = () => {
+  return (
+    <div className="restroCard">
+      <img
+        className="restroLogo"
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/s8lriomkmnw8zogjdqh2"
+        alt="Error in loading Image"
+      ></img>
+      <h3>Mathrusri Tiffins</h3>
+      <h4>Indian, Pan Asian</h4>
+      <div className="restroDetails">
+        <span>4.3*</span>
+        <span>20 mins</span>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="searchRestro">
+        <input
+          className="searchTxt"
+          type="search"
+          placeholder="Search restuarant by name"
+        ></input>
+        <input className="searchBtn" type="button" value="Search"></input>
+      </div>
+      <div className="restroContainer">
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+        <Restro></Restro>
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="headerDiv">
+      <div className="logoContainer">
+        <img
+          className="logo"
+          src="https://www.designevo.com/res/templates/thumb_small/simple-cup-crisp-cookie.png"
+          alt="Failed to load the Image"
+        ></img>
+      </div>
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Profile</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="appLayout">
+      {/* Need to place header, body & footer Component  */}
+      <Header></Header>
+      <Body></Body>
+    </div>
+  );
+};
+
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-rootElement.render(parent);
+
+rootElement.render(<AppLayout></AppLayout>);
